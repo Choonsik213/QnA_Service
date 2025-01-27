4b2abff301fb2754a5eb04a6761f2c834075d165
Choonsik213 <tjcodud6525@naver.com>
2025-01-27 16:30:45 +0900

    데이터 수정하기

--- src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
+++ src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class QnaServiceApplicationTests {
@@ -92,5 +93,23 @@ class QnaServiceApplicationTests {
         assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
 
+    /*
+    [SQL]
+    UPDATE question
+    SET content = ?,
+    create_date = ?,
+    subject = ?,
+    WHERE id = ?;
+    */
+    @Test
+    @DisplayName("데이터 수정하기")
+    void t007() {
+        // SELECT * FROM question WHERE id=1;
+        Optional<Question> oq = questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        questionRepository.save(q);
+    }
 
 }
