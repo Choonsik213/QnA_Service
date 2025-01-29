5da0b9ab5fad0591ba5940826b9baf66f21ecc96
Choonsik213 <tjcodud6525@naver.com>
2025-01-29 11:43:26 +0900

    질문을 통해 답변 찾기

--- src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
+++ src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -215,6 +216,21 @@ class QnaServiceApplicationTests {
         assertTrue(oa.isPresent());
         Answer a = oa.get();
         assertEquals(2, a.getQuestion().getId());
+    }
+
+    @Transactional  // 테스트코드에서는 Transactional을 붙여줘야한다.
+    @Test
+    @DisplayName("짊문을 통해 답변 찾기")
+    void t011() {
+        // SQL : SELECT * FROM question WHERE id = 2;
+        Optional<Question> oq = questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 
 }
