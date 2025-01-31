b05f06f618c3709d3f7465ca407634be8af71981
Choonsik213 <tjcodud6525@naver.com>
2025-01-31 10:24:42 +0900

    타임리프 연결

--- src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
+++ src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -227,22 +225,22 @@ class QnaServiceApplicationTests {
      * ON Q.id = A.question_id
      * WHERE Q.id = ?
      */
-    @Transactional  // 테스트코드에서는 Transactional을 붙여줘야한다.
-    @Test
-    @DisplayName("짊문을 통해 답변 찾기")
-    @Rollback(false)    // 테스트 메서드가 끝난 후에도 트랜잭션이 롤백되지 않고 커밋된다.
-    void t011() {
-        System.out.println("실행됨");
-        // SQL : SELECT * FROM question WHERE id = 2;
-        Optional<Question> oq = questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-        //테스트 환경에서는 get해서 가져온 뒤 DB연결을 끊음
-
-        // SQL : SELECT* FROM answer WHERE question_id = 2;
-        List<Answer> answerList = q.getAnswerList();    // DB통신이 끊긴 뒤 answer을 가져옴 ->실패
-
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
-    }
+//    @Transactional  // 테스트코드에서는 Transactional을 붙여줘야한다.
+//    @Test
+//    @DisplayName("짊문을 통해 답변 찾기")
+//    @Rollback(false)    // 테스트 메서드가 끝난 후에도 트랜잭션이 롤백되지 않고 커밋된다.
+//    void t011() {
+//        System.out.println("실행됨");
+//        // SQL : SELECT * FROM question WHERE id = 2;
+//        Optional<Question> oq = questionRepository.findById(2);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//        //테스트 환경에서는 get해서 가져온 뒤 DB연결을 끊음
+//
+//        // SQL : SELECT* FROM answer WHERE question_id = 2;
+//        List<Answer> answerList = q.getAnswerList();    // DB통신이 끊긴 뒤 answer을 가져옴 ->실패
+//
+//        assertEquals(1, answerList.size());
+//        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+//    }
 }
