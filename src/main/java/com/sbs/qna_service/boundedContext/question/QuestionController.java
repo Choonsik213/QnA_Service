20203c9d257cdba3775d331b188be5852ea4b0ae
Choonsik213 <tjcodud6525@naver.com>
2025-02-02 18:22:57 +0900

    질문 상세 컨트롤러 만들기

--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,5 +20,10 @@ public class QuestionController {
 
         model.addAttribute("questionList", questionList);   // questionList라는 이름으로 화면에 뿌려준다.
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
     }
 }
